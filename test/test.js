c9880d7f93e8905c3307b7a6432a3615fb9df976
Aleksandar Maric <aleksandar.maric@propulsionapp.com>
2014-09-10 13:52:30 +0200

    chenge test.js

--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@ var app = require('../app.js');
  
 describe('GET /', function() {
   it('respond with hello world', function(done) {
-    request(app).get('/').expect('hello jenkins4', done);
+    request(app).get('/').expect('hello jenkins', done);
   });
 });
 
